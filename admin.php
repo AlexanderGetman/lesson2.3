8f04a823b6a97b79e9f7a3242712b61f05728089
AlexanderGetman <40514260+AlexanderGetman@users.noreply.github.com>
2018-08-05 15:08:18 +1000

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,14 +1,15 @@
 <?php
 
-$dest = __DIR__.'/test'.'/test'.$_FILES['test']['name'];
-
-if (move_uploaded_file($_FILES['test']['tmp_name'], $dest)) {
-    if(isset($_POST['submit'])){
-        header('Location: list.php');
-        exit;
+if(isset($_POST['submit']) && isset($_FILES)) {
+    $dest = __DIR__.'/test'.'/test'.$_FILES['test']['name'];
+    if (move_uploaded_file($_FILES['test']['tmp_name'], $dest)) {
+        if(isset($_POST['submit'])){
+            header('Location: list.php');
+            exit;
+        }
+    } else {
+        echo "Файл не загружен";
     }
-} else {
-    echo "Файл не загружен";
 }
 
 
